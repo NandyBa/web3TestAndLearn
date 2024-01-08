3c6709bb7a71953071e1d4ab0df8985b7a280fdd
Nandy Bâ <nandyba@yahoo.fr>
2024-01-08 23:49:18 +0100

    feature: remove tests related to Aave v2

--- test/AaveAddressBook.sol
+++ test/AaveAddressBook.sol
@@ -12,42 +12,6 @@ contract AaveAddressBookTest is Test {
     vm.createSelectFork(vm.envString("MAINNET_RPC_URL"));
   }
 
-  // function testPoolAddressProviderIsCorrect() public {
-  //   assertEq(
-  //     address(AaveV3Ethereum.POOL_ADDRESSES_PROVIDER),
-  //     address(0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5)
-  //   );
-  // }
-
-  // function testPoolAddressIsCorrect() public {
-  //   assertEq(address(AaveV3Ethereum.POOL), address(0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9));
-  // }
-
-  // function testPoolConfiguratorIsCorrect() public {
-  //   assertEq(
-  //     address(AaveV3Ethereum.POOL_CONFIGURATOR),
-  //     address(0x311Bb771e4F8952E6Da169b425E7e92d6Ac45756)
-  //   );
-  // }
-
-  // function testOracleIsCorrect() public {
-  //   assertEq(address(AaveV3Ethereum.ORACLE), address(0xA50ba011c48153De246E5192C8f9258A2ba79Ca9));
-  // }
-
-  // function testAaveProtocolDataProviderIsCorrect() public {
-  //   assertEq(
-  //     address(AaveV3Ethereum.AAVE_PROTOCOL_DATA_PROVIDER),
-  //     address(0x057835Ad21a177dbdd3090bB1CAE03EaCF78Fc6d)
-  //   );
-  // }
-
-  // function testCollectorIsCorrect() public {
-  //   assertEq(
-  //     address(AaveV3Ethereum.COLLECTOR),
-  //     address(0x464C71f6c2F760DdA6093dCB91C24c39e5d6e18c)
-  //   );
-  // }
-
   function testGetPool() public {
     POOL_ADDRESSES_PROVIDER = IPoolAddressesProvider(0x2f39d218133AFaB8F2B819B1066c7E434Ad94E9e);
 
